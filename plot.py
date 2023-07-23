63defdc99d49748c469acec9eb1774867f141f3a
wyatt <wyatt_liu@hotmail.com>
2023-07-23 08:27:39 -0400

    save fig from args

--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_line_segments(csv_file):
+def plot_line_segments(csv_file, fig_file_name):
     df = pd.read_csv(csv_file)
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
     print(df)
@@ -41,9 +43,16 @@ def plot_line_segments(csv_file):
     plt.grid(True)
     #plt.gca().set_xlim(0, 6)
     #plt.gca().set_ylim(0, 10)
-    plt.show()
-
+    #plt.show()
+    plt.savefig(fig_file_name)
 
 if __name__ == "__main__":
-    csv_file_path = 'sample_no_style.csv'
-    plot_line_segments(csv_file_path)
+    # read arg 1 as csv file name if arg 2 is not given use the input as the save file name (change extension to .png)
+    if len(sys.argv) == 2:
+        csv_file_name = sys.argv[1]
+        save_file_name = csv_file_name.split('.')[0] + '.png'
+        plot_line_segments(csv_file_name, save_file_name)
+    elif len(sys.argv) == 3:
+        plot_line_segments(sys.argv[1], sys.argv[2])
+    else:
+        print("Usage: python plot.py <csv_file_name> <save_file_name>")
