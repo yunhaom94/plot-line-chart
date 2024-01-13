b2569f056e081edc0ad5a19291f380a9f6bc3fab
yunhaom94 <yunhaom94@outlook.com>
2024-01-13 03:26:30 -0500

    update

---
+++ plt_tp_vs_lt.py
@@ -0,0 +1,82 @@
+import sys
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_line_segments(csv_file1, csv_file2, fig_file_name):
+
+    # Create a new plot
+    fig, axs  = plt.subplots(1, 2, figsize=(20, 5))
+
+    plot_lines(csv_file1, axs[0], 0)
+    plot_lines(csv_file2, axs[1], 1)
+    plt_to_file(fig, fig_file_name)
+
+
+def plot_lines(csv_file, ax, left_or_right):
+    df = pd.read_csv(csv_file)
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    print(df)
+
+    for i in range(0, len(df.columns), 3):
+        series_x = df.columns[i]
+        series_y = df.columns[i+1]
+        series_error = df.columns[i+2]
+        style = df[series_y].dropna().iloc[-3:].tolist()
+        x_cords = df[series_x].dropna().tolist()
+        y_cords = df[series_y].dropna().iloc[:-3].tolist()
+        error_values = df[series_error].dropna().tolist()
+        x_cords = list(map(float, x_cords))
+        y_cords = list(map(float, y_cords))
+        error = list(map(float, error_values))
+        has_style = 1
+        if len(y_cords) != len(x_cords):
+            y_cords = df[series_y].dropna().tolist()
+            has_style = 0
+        print("X:")
+        print(x_cords)
+        print("Y:")
+        print(y_cords)
+        print("Error:")
+        print(error)
+        
+
+        if has_style:
+            print("Style:")
+            print(style)
+
+            ax.plot(x_cords, y_cords, label=series_y, linestyle=style[0], marker=style[1], color=style[2])
+            ax.errorbar(x_cords, y_cords, yerr=error, linestyle=style[0], marker=style[1], color=style[2], capsize=2, elinewidth=0.5)
+
+        else:
+            raise Exception("No style provided")
+        # set aspect ratio
+    
+    for l in ax.lines:
+        l.set_linewidth(0.9)
+        l.set_alpha(0.7)
+
+
+    ax.grid(True)
+    ax.set_box_aspect(0.5)
+    ax.set_xlim(left=0)
+    ax.set_xlabel("Throughput (ops/s)")
+    ax.set_ylim(bottom=0)
+
+    if (left_or_right == 0):
+        legend = ax.legend(bbox_to_anchor=(1.05, 1.25), ncol=9, loc='upper center') 
+        ax.set_ylabel("Latency (ms)")
+        ax.set_title("PN Counter", y=-0.01)
+    else:
+        ax.set_title("OR Set", y=-0.01)
+
+    if (left_or_right == 1):
+        ax.set_xticks([40000, 80000, 120000, 160000, 200000])
+
+def plt_to_file(fig, fig_file_name):
+    fig.savefig(fig_file_name, dpi=300, format="pdf", bbox_inches="tight")
+
+if __name__ == "__main__":
+    plot_line_segments("results/pnc_tp_vs_lt.csv", "results/orset_tp_vs_lt.csv", "results/tp_vs_lt.pdf")
+
